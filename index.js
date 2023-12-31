548f2a75e75b8e1faf59265dbed04d3a09dd0a1e
Dr Who Baker <tbaker.bhb@gmail.com>
2023-12-31 22:32:45 +0000

    added a stick button and dealers draw

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 let player = {
     name: prompt("Please enter your name : "),
-    chips: 0
+    chips: []
 }
 let cards = []
 let sum = 0
@@ -11,7 +11,7 @@ let message = "";
 let messageEl = document.getElementById("message-el");
 let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
-
+let dealersEl = document.getElementById("dealers-el")
 
 let playerEl = document.getElementById("player-el")
 playerEl.textContent = player.name + ": £" + player.chips
@@ -29,6 +29,8 @@ function getRandomCard() {
 }
 
 function startGame() {
+    cards = [];
+    sum = 0;
     isAlive = true
     let firstCard = getRandomCard();
     let secondCard = getRandomCard();
@@ -39,20 +41,26 @@ function startGame() {
 
 function renderGame() {
     cardsEl.textContent = "Cards: ";
+    
     for (let i = 0; i < cards.length; i++) {
         cardsEl.textContent += cards[i] + " "
     }
 
     sumEl.textContent = "Sum: " + sum;
+
     if (sum <= 20) {
         message = "Do you want to draw a new card?" ;
     } else if (sum === 21) {
         message = "You've got Blackjack!" ;
-        player.chips += 21
-        hasBlackJack = true;
+        player.chips += 21 
+        hasBlackJack = true; 
+        sum = 0;
+        cards = []
     } else {
         message = "You're out of the game!"  ;
         isAlive = false
+        sum = 0;
+        cards = []
     }
     
     messageEl.textContent = message
@@ -68,3 +76,15 @@ function newCard() {
     }
 }
 
+function stick() {
+    let dealersNumber = getRandomCard() * 2;
+    dealersEl.textContent = "Dealers Draw is: " + dealersNumber;
+    if (dealersNumber <= sum) {
+        player.chips += 9;
+        cards = [];
+        sum = 0
+    } else {
+        cards = [];
+        sum = 0
+    }
+}
